07456377f61fdb1d93526b1cbb74970c4beeb81b
Andres Suarez~ <santiagosuarez.99@hotmail.com>
2023-07-09 18:59:43 -0500

    Arreglo de bugs

--- desafio.js
+++ desafio.js
@@ -1,11 +1,10 @@
 class ProductManager {
+    constructor() {
+        this.products = [];
+    }
 
-    constructor(){
-        this.products = []
-    };
-
-    addProducts(title, description, price, thumbnail, code, stock){
-        const codeRepetido = this.products.find(p => p.code == code)
+    addProduct(title, description, price, thumbnail, code, stock) {
+        const codeRepetido = this.products.find(p => p.code === code);
         const product = {
             title,
             description,
@@ -13,38 +12,35 @@ class ProductManager {
             thumbnail,
             code,
             stock,
-            id: this.products.length + 1
+            id: this.products.length + 1,
+        };
+        if (title && description && price && thumbnail && code && stock !== undefined && !codeRepetido) {
+            this.products.push(product);
+        } else {
+            console.log('Complete all fields');
         }
-        if(title && description && price && thumbnail && code && stock != undefined && !codeRepetido){
-            this.products.push(product)
-        }else{
-            console.log('Complete all fields')
-        }
-    };
-
-    getProducts(){
-        return this.products
     }
 
-    getProductsById(id){
+    getProducts() {
+        return this.products;
+    }
 
-        const productById = this.products.find(p => p.id == id)
+    getProductById(id) {
+        const productById = this.products.find(p => p.id === id);
         if (productById) {
-            return productById
+            return productById;
         }
-        return console.error("Not Found")
-    };
-
-};
+        console.error('Not Found');
+    }
+}
 
 const productos1 = new ProductManager()
 productos1.addProduct('RedBul Red','Energizante',6300,'url imagen','5555',100)
 productos1.addProduct('RedBul Green','Energizante',7500,'url imagen','1042350',0)
 productos1.addProduct('RedBul Black','Energizante',6300,'url imagen','58455',100)
 
-const productoId1 = productos1.getProductsById(2)
+const productById1 = productos1.getProductById(1);
+const productById5 = productos1.getProductById(5);
 
-productos1.getProductsById(5)
-
-console.log(productos1.getProducts())
-console.log(productoId1)
+console.log(productos1.getProducts());
+console.log(productById1);
